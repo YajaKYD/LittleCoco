d0a3266178f6f30328236c999cd890362e51c3bd
index.html 추가 <itct@itctui-iMac.local>
2017-06-07 13:28:37 +0900

    4_1 말풍선문제

--- Assets/_Script/NPC_Click_to_Talk/Star_TextPosition.cs
+++ Assets/_Script/NPC_Click_to_Talk/Star_TextPosition.cs
@@ -10,6 +10,7 @@ public class Star_TextPosition : MonoBehaviour {
 	TurnOnOffItemList itemlist;
 
 	public RectTransform Star_pos;
+	private bool findIC;
 
 	void Awake () {
 		s = GetComponent<RectTransform> ();
@@ -20,12 +21,27 @@ public class Star_TextPosition : MonoBehaviour {
 //		itemlist = _ic.GetComponentInChildren<TurnOnOffItemList> ();
 //		for (int i = 0; i < _ic._item_list.Length; i++) {
 //			if (_ic._item_name_list [i] == "Star") {
-//				Star_pos = GameObject.Find ("Item_button_" + i).GetComponent<RectTransform> ();
+//					Star_pos = GameObject.Find ("Item_button_" + i).GetComponent<RectTransform> ();
+//				}
 //			}
-//		}
 //	}
 		
 	void Update () {
+//		if (findIC == false) {
+//			Debug.Log (GameObject.FindWithTag ("Item_Canvas").name);
+//			_ic = GameObject.FindWithTag ("Item_Canvas").GetComponent<Item_Controller> ();
+//			itemlist = _ic.GetComponentInChildren<TurnOnOffItemList> ();
+//			if (Star_pos == null) {
+//				for (int i = 0; i < _ic._item_list.Length; i++) {
+//					if (_ic._item_name_list [i] == "Star") {
+//						Star_pos = GameObject.Find ("Item_button_" + i).GetComponent<RectTransform> ();
+//					}
+//				}
+//			}
+//			Debug.Log ("4 find IC " + findIC);
+//			findIC = true;
+//		}
+
 		Debug.Log (GameObject.FindWithTag ("Item_Canvas").name);
 		_ic = GameObject.FindWithTag ("Item_Canvas").GetComponent<Item_Controller> ();
 		itemlist = _ic.GetComponentInChildren<TurnOnOffItemList> ();
