ce0bb6e5ab0ec09685891c118a18aec4b9d5462a
전호균 <arteareas13@gmail.com>
2017-06-19 16:23:16 +0900

    작업 시작 전 커밋

--- Assets/_Script/Stage4/Stage4_3_gum.cs
+++ Assets/_Script/Stage4/Stage4_3_gum.cs
@@ -7,15 +7,21 @@ public class Stage4_3_gum : MonoBehaviour {
 	public GameObject gumPuzzle;
 	public GameObject gumPuzzlePrefab;
 	private GameObject item_Canvas;
+	private Stage4_3_GameController controller;
 
 	void Start () {
 		item_Canvas = GameObject.FindWithTag ("Item_Canvas");
+		controller = GameObject.Find ("Stage4_3_GameController").GetComponent<Stage4_3_GameController> ();
 	}
 
 	void OnTriggerEnter2D(){
 		if (gumPuzzle == null) {
 			gumPuzzle = Instantiate (gumPuzzlePrefab, Vector3.zero, Quaternion.identity) as GameObject;
 			gumPuzzle.transform.SetParent (item_Canvas.transform, false);
+			controller.emptyPos [0] = gumPuzzle.transform.GetChild (1);
+			controller.emptyPos [1] = gumPuzzle.transform.GetChild (2);
+			controller.puzzlePiece [0] = gumPuzzle.transform.GetChild (3).gameObject;
+			controller.puzzlePiece [1] = gumPuzzle.transform.GetChild (4).gameObject;
 		}
 	}
 
