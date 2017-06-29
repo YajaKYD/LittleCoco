d094219aa3f167ac1c814525f5b0e91a2220de83
전호균 <arteareas13@gmail.com>
2017-06-29 18:01:16 +0900

    stage 4_6 round 2 -ing

--- Assets/_Script/Stage4/Stage4_6_GameController.cs
+++ Assets/_Script/Stage4/Stage4_6_GameController.cs
@@ -6,23 +6,30 @@ using UnityEngine.UI;
 public class Stage4_6_GameController : MonoBehaviour {
 
 	public Transform fraudPanel;
-	private bool q22_0;
+	private bool q22_0, q22_1;
 	public int[] number = new int[2];
 	public int scorePlayer, scoreNeogulman;
-	public GameObject neogulman, neogulmanPrefab;
+	//public GameObject figure, neogulmanPrefab, starPrefab;
+	public Sprite neogulman, star;
 	public bool clickable;
 	public int roundNo = 0;
+	private Text roundBoard;
 
 	public GameObject[] slot;
+	private GameObject tempSlot;
+	public GameObject[] buttons;
 	private Vector3[] velocity;
 	public float smoothTime = 0.3f;
 	public static bool getTempTransform;
 	private Vector3[] tempPos;
 	public bool mixDone;
 	public int mixTimes;
+	private GameObject itemCanvas;
 
 	void Start () {
+		itemCanvas = GameObject.FindWithTag ("Item_Canvas");
 		Stage4_Controller.q [20] = true;
+		roundBoard = GameObject.Find ("Round").GetComponent<Text> ();
 		returnRandom ();
 
 		tempPos = new Vector3[2];
@@ -38,48 +45,172 @@ public class Stage4_6_GameController : MonoBehaviour {
 			Q21_popupGame ();
 		} else if (Stage4_Controller.q [21] && !Stage4_Controller.q [22]) {
 			Q22_startGame ();
+		} else if (Stage4_Controller.q [22]) {
+			Debug.Log ("q22 complete");
 		}
 	}
 
 	void Q21_popupGame(){
 		//conversation
-		//Pop up Game UI
-		ShowMenu(fraudPanel);
+		itemCanvas.SetActive(false);
+		ShowMenu(fraudPanel); // Pop up Game UI
 		Stage4_Controller.q [21] = true;
 	}
 
 	void Q22_startGame(){
+		switch(roundNo){
+		case 1:
+			Mix (20);
+			break;
+
+		case 2:
+			PlayerMix ();
+			break;
+
+		case 3:
+			Mix (20);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	void Mix(int times){
 		if (q22_0) {
 			if (mixDone) {
 				returnRandom ();
 			} else if (!mixDone) {
 				Mix (number [0], number [1]);
-				if (mixTimes >= 20) { // Mix Done Completely
+				if (mixTimes >= times) { // Mix Done Completely
 					clickable = true;
-					Stage4_Controller.q [22] = true;
+					q22_0 = false;
 				}
 			}
 		}
 	}
 
-	public void tempComplete(){
-		HideNeogulman ();
-		q22_0 = true;
-	}
+	void Mix(int a, int b){
+		//button deactive
+		if (!getTempTransform) {
+			tempPos [0] = slot [a].transform.position;
+			tempPos [1] = slot [b].transform.position;
+			getTempTransform = true;
+		}
+		slot [a].transform.position = Vector3.SmoothDamp (slot[a].transform.position, tempPos[1], ref velocity[0], smoothTime);
+		slot [b].transform.position = Vector3.SmoothDamp (slot[b].transform.position, tempPos[0], ref velocity[1], smoothTime);
 
-	public void Restart(){
-		Destroy (neogulman);
-		for (int i = 0; i < slot.Length; i++) {
-			slot [i].GetComponent<Stage4_6_Fraud> ().neogulmanIn = false;
-			slot [i].GetComponent<Image> ().enabled = true;
+		if (Vector3.Distance (slot [a].transform.position, tempPos [1]) <= 0.1f && Vector3.Distance(slot[b].transform.position, tempPos[0]) <= 0.1f) {
+			//button active
+			mixTimes++;
+			mixDone = true;	
+			getTempTransform = false;
+			Debug.Log (mixDone + ", " + mixTimes);
+			tempSlot = slot [a];
+			slot [a] = slot [b];
+			slot [b] = tempSlot;
 		}
 	}
 
-	public void HideNeogulman(){
-		int answer = Random.Range (0, 3);
-		neogulman = Instantiate (neogulmanPrefab, slot[answer].transform.position, Quaternion.identity) as GameObject;
-		neogulman.transform.SetParent (slot [answer].transform);
-		slot [answer].GetComponent<Stage4_6_Fraud> ().neogulmanIn = true;
+
+	void PlayerMix(){
+		if (!q22_1) {
+			for (int i = 0; i < buttons.Length; i++) {
+				buttons [i].SetActive (true);
+			}
+			q22_1 = true;
+			mixDone = true;
+		} else {
+			if (!mixDone) {
+				Mix (number [0], number [1]);
+			}
+		}
+	}
+		
+	public void MixButton(int a){
+		switch (a) {
+		case 0:
+			number [0] = 0;
+			number [1] = 1;
+			break;
+		case 1:
+			number [0] = 0;
+			number [1] = 2;
+			break;
+		case 2:
+			number [0] = 1;
+			number [1] = 2;
+			break;
+		default:
+			number [0] = 0;
+			number [1] = 0;
+			break;
+		}
+		mixDone = false;
+		mixTimes++;
+		StartCoroutine ("ActivateButtons"); // might be changed
+	}
+
+	IEnumerator ActivateButtons(){
+		for (int i = 0; i < buttons.Length; i++) {
+			buttons [i].GetComponent<Button> ().enabled = false;
+		}
+		yield return new WaitForSeconds (0.5f); // time scale?
+		for (int i = 0; i < buttons.Length; i++) {
+			buttons [i].GetComponent<Button> ().enabled = true;
+		}
+	}
+
+	public void tempComplete(){
+		Reset (3);
+		switch (roundNo) {
+		case 1:
+			CopyOneself(neogulman);
+			break;
+		case 2:
+			CopyOneself (star);
+			break;
+		case 3: 
+			CopyOneself(neogulman);
+			break;
+		default:
+			break;
+		}
+		q22_0 = true;
+	}
+
+	public void Reset(int _roundNo){
+		for (int i = 0; i < slot.Length; i++) {
+			slot [i].GetComponent<Stage4_6_Fraud> ().neogulmanIn = false;
+		}
+		mixTimes = 0;
+		clickable = false;
+		roundNo++;
+		roundBoard.text = roundNo + " Round";
+		for (int i = 0; i < buttons.Length; i++) {
+			buttons [i].SetActive (false);
+		}
+		if (roundNo > _roundNo) {
+			Stage4_Controller.q [22] = true;
+		}
+	}
+
+	void CopyOneself(Sprite figure){
+		slot [1].GetComponent<Image> ().sprite = figure;
+		slot [1].GetComponent<Stage4_6_Fraud> ().neogulmanIn = true;
+		//conversation
+		//appear effect
+		slot[0].GetComponent<Image> ().sprite = figure;
+		slot[2].GetComponent<Image> ().sprite = figure;
+	}
+
+	public void ShowAnswer(){
+		for (int i = 0; i < slot.Length; i++) {
+			if (!slot [i].GetComponent<Stage4_6_Fraud> ().neogulmanIn) {
+				slot [i].GetComponent<Image> ().sprite = null;
+				//disappear effect
+			}
+		}
 	}
 
 	int[] returnRandom(){
@@ -88,7 +219,6 @@ public class Stage4_6_GameController : MonoBehaviour {
 		while (number [0] == number [1]) {
 			number [1] = Random.Range (0, 3);
 		}
-		Debug.Log (number [0] + ", " + number [1]);
 		mixDone = false;
 		return number;
 	}
@@ -98,25 +228,6 @@ public class Stage4_6_GameController : MonoBehaviour {
 		menu.GetComponent<Animator>().SetBool("show", true);
 	}
 		
-	public void Mix(int a, int b){
-		if (!getTempTransform) {
-			tempPos [0] = slot [a].transform.position;
-			tempPos [1] = slot [b].transform.position;
-			getTempTransform = true;
-		}
-		//Debug.Log ("tempPos[0] = " + tempPos [0] + " = " + slot[b].transform.position);
-		//Debug.Log (Vector3.Distance (slot [a].transform.position, tempPos [1]));
-		//Debug.Log ("tempPos[1] = " + tempPos [1] + " = " + slot[a].transform.position);
 
-		slot [a].transform.position = Vector3.SmoothDamp (slot[a].transform.position, tempPos[1], ref velocity[0], smoothTime);
-		slot [b].transform.position = Vector3.SmoothDamp (slot[b].transform.position, tempPos[0], ref velocity[1], smoothTime);
-
-		if (Vector3.Distance (slot [a].transform.position, tempPos [1]) <= 0.1f && Vector3.Distance(slot[b].transform.position, tempPos[0]) <= 0.1f) {
-			mixTimes++;
-			mixDone = true;	
-			getTempTransform = false;
-			Debug.Log (mixDone + ", " + mixTimes);
-		}
-	}
 
 }
